d0946e8ea871b08eda48f1de1c1678c929804ba4
Fede <134746653+AndieCoding@users.noreply.github.com>
2026-02-16 16:33:08 -0300

    Optimizando MP

--- src/routes/payments.js
+++ src/routes/payments.js
@@ -6,6 +6,7 @@ const router = express.Router();
 router.post('/mp/create_preference', createPreference);
 router.post('/transfer/confirm', confirmTransferOrder);
 router.post('/checkout/result', async (req, res) => {
+    res.status(200).send('OK');
     console.log('Checkout result body:', req.body);
     console.log('Checkout result req:', req);
 });
