989e968c5c97a350e5b0f3983d7da7454d771e84
Fede <134746653+AndieCoding@users.noreply.github.com>
2026-02-18 11:47:48 -0300

    Optimización para Aiven

--- src/config/db.js
+++ src/config/db.js
@@ -10,10 +10,10 @@ const pool = mysql.createPool({
     port: process.env.DB_PORT || 3306,
     ssl: { rejectUnauthorized: false },
     waitForConnections: true,
-    connectionLimit: 10,
+    connectionLimit: 5,
     queueLimit: 0,
-    idleTimeout: 120000,
-    maxIdle: 5,
+    idleTimeout: 10000,
+    maxIdle: 1,
     enableKeepAlive: true,
     keepAliveInitialDelay: 0,
 
